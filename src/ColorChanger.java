98538c1119acdbc1d324021582bce994e245b4f9
BCshirdavani <beau.shirdavani@bellevuecollege.edu>
2018-01-08 21:33:49 -0800

    added TRUE RGB test
    I now see what RGB value I SHOULD have at a given pixel, and can compare that to what I actually got...

--- src/ColorChanger.java
+++ src/ColorChanger.java
@@ -1,5 +1,6 @@
 
 import java.awt.*;
+import java.awt.color.ColorSpace;
 import java.io.*;
 
 import javax.imageio.*;
@@ -25,11 +26,11 @@ public class ColorChanger {
                 // grab the RGB value of a pixel at x,y coord in the image
                 int rgb = raw.getRGB(x, y);
                 // extract the red value
-                int red = ((rgb >> 8) & 000000000000000011111111);
+                int red = ((rgb >> 16) & 000000000000000011111111);
                 // extract the green value
-                int green = ((rgb >> 16) & 000000000000000011111111);
+                int green = ((rgb >> 24) & 000000000000000011111111);
                 // extract the blue value
-                int blue = ((rgb >> 24) & 000000000000000011111111); //000000000000000000000000
+                int blue = ((rgb >> 32) & 000000000000000011111111); //000000000000000000000000
                 // use Color.RGBtoHSB() method to convert RBG to HSV
                 hsb = Color.RGBtoHSB(red,green,blue,null);
                 float old_hue = hsb[0];
@@ -39,13 +40,28 @@ public class ColorChanger {
                 newRGB = Color.HSBtoRGB(old_hue,old_sat,old_brt);
                 // set the new RGB value to a pixel at x,y coordinates of the image
                 processed.setRGB(x, y, newRGB);
+
+                // test at pixel 1,1 --------------------------------------------------- TEST
                 if (x == 1 && y == 1){
+                    // RGB values from my shift and mask method:
                     System.out.println("Old Red was: " + red);
                     System.out.println("Old Green was: " + green);
                     System.out.println("Old BLue was: " + blue);
+                    // their HSB values are then:
                     System.out.println("Hue is: " + hsb[0]);
                     System.out.println("Sat is: " + hsb[1]);
                     System.out.println("Brightness is: " + hsb[2]);
+                    // but the TRUE RGB values were actually:
+                    Color c = new Color(raw.getRGB(x,y));
+                    int TRUE_red = c.getRed();
+                    int TRUE_green = c.getGreen();
+                    int TRUE_blue = c.getBlue();
+                    int TRUE_alpha = c.getAlpha();
+                    System.out.println("TRUE Red was: " + TRUE_red);
+                    System.out.println("TRUE Green was: " + TRUE_green);
+                    System.out.println("TRUE BLue was: " + TRUE_blue);
+                    System.out.println("TRUE Alpha was: " + TRUE_alpha);
+                //----------------------------------------------------------------------
                 }
             }
 
